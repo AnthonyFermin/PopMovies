cc2eae9598381dcfcdefa5d46c727cfe5f7fee7d
Anthony Fermin <afermin@fuzzproductions.com>
2017-09-08 16:26:03 -0400

    Save state for Trailers and Reviews Views

--- app/src/main/java/anthonyfdev/com/popmovies/discovery/ReviewsView.java
+++ app/src/main/java/anthonyfdev/com/popmovies/discovery/ReviewsView.java
@@ -3,6 +3,8 @@ package anthonyfdev.com.popmovies.discovery;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -31,6 +33,7 @@ public class ReviewsView extends FrameLayout {
     private TextView tvEmptyText;
     private View progressBar;
     private ReviewAdapter reviewAdapter;
+    private int scrollPosition;
 
     public ReviewsView(@NonNull Context context) {
         super(context);
@@ -64,6 +67,33 @@ public class ReviewsView extends FrameLayout {
         rvReviews.setAdapter(reviewAdapter);
     }
 
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        RecyclerView.LayoutManager layoutManager = rvReviews.getLayoutManager();
+        if(layoutManager != null && layoutManager instanceof LinearLayoutManager){
+            scrollPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+        }
+        SavedState newState = new SavedState(superState);
+        newState.scrollPosition = scrollPosition;
+        return newState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        super.onRestoreInstanceState(state);
+        if(state != null && state instanceof SavedState){
+            scrollPosition = ((SavedState) state).scrollPosition;
+            RecyclerView.LayoutManager layoutManager = rvReviews.getLayoutManager();
+            if(layoutManager != null){
+                int count = layoutManager.getChildCount();
+                if(scrollPosition != RecyclerView.NO_POSITION && scrollPosition < count){
+                    layoutManager.scrollToPosition(scrollPosition);
+                }
+            }
+        }
+    }
+
     public void setReviews(List<Review> reviews) {
         if (reviews != null && !reviews.isEmpty()) {
             reviewAdapter.setReviews(reviews);
@@ -81,5 +111,34 @@ public class ReviewsView extends FrameLayout {
 
     public RecyclerView getRvReviews() {
         return rvReviews;
+    }
+
+    static class SavedState extends android.view.View.BaseSavedState {
+        public int scrollPosition;
+        SavedState(Parcel in) {
+            super(in);
+            scrollPosition = in.readInt();
+        }
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeInt(scrollPosition);
+        }
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
     }
 }
--- app/src/main/java/anthonyfdev/com/popmovies/discovery/TrailersView.java
+++ app/src/main/java/anthonyfdev/com/popmovies/discovery/TrailersView.java
@@ -3,6 +3,8 @@ package anthonyfdev.com.popmovies.discovery;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -30,6 +32,7 @@ public class TrailersView extends FrameLayout {
     private RecyclerView rvTrailers;
     private View progressBar;
     private TrailerAdapter trailerAdapter;
+    private int scrollPosition;
 
     public TrailersView(@NonNull Context context) {
         super(context);
@@ -63,6 +66,33 @@ public class TrailersView extends FrameLayout {
         rvTrailers.setAdapter(trailerAdapter);
     }
 
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        RecyclerView.LayoutManager layoutManager = rvTrailers.getLayoutManager();
+        if(layoutManager != null && layoutManager instanceof LinearLayoutManager){
+            scrollPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+        }
+        SavedState newState = new SavedState(superState);
+        newState.scrollPosition = scrollPosition;
+        return newState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        super.onRestoreInstanceState(state);
+        if(state != null && state instanceof SavedState){
+            scrollPosition = ((SavedState) state).scrollPosition;
+            RecyclerView.LayoutManager layoutManager = rvTrailers.getLayoutManager();
+            if(layoutManager != null){
+                int count = layoutManager.getChildCount();
+                if(scrollPosition != RecyclerView.NO_POSITION && scrollPosition < count){
+                    layoutManager.scrollToPosition(scrollPosition);
+                }
+            }
+        }
+    }
+
     public void setTrailers(List<Trailer> trailers) {
         if (trailers != null && !trailers.isEmpty()) {
             trailerAdapter.setTrailers(trailers);
@@ -80,5 +110,34 @@ public class TrailersView extends FrameLayout {
 
     public RecyclerView getRvTrailers() {
         return rvTrailers;
+    }
+
+    static class SavedState extends android.view.View.BaseSavedState {
+        public int scrollPosition;
+        SavedState(Parcel in) {
+            super(in);
+            scrollPosition = in.readInt();
+        }
+        SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            super.writeToParcel(dest, flags);
+            dest.writeInt(scrollPosition);
+        }
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
     }
 }
